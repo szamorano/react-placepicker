00d36bb951cfa572dd20cd2ed31521758a630914
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-18 19:29:58 -0400

    An infinite loop

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { sortPlacesByDistance } from "./loc.js";
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
 
   navigator.geolocation.getCurrentPosition((position) => {
@@ -18,6 +19,7 @@ function App() {
       position.coords.latitude,
       position.coords.longitude,
     );
+    setAvailablePlaces(sortedPlaces);
   });
 
   function handleStartRemovePlace(id) {
@@ -72,7 +74,7 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
           onSelectPlace={handleSelectPlace}
         />
       </main>
